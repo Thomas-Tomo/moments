a2d78b7bf54f669c8413b6f3c06ae81921d8715a
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-11 19:22:21 +0000

    Add sign up functionality

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://moments-thomas-0a5a0566b8ac.herokuapp.com/'
+axios.defaults.baseURL = 'https://drf-api-thomas-48cbbb8b8839.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
