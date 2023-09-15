ad144bd16a7f4b04c41a001cadd2b488f42e9860
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-15 22:10:58 +0000

    Add popular profiles component

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -16,6 +16,7 @@ import { axiosReq } from "../../api/axiosDefaults";
 import NoResults from "../../assets/no-results.png";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
 
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
@@ -67,12 +68,11 @@ function PostsPage({ message, filter = "" }) {
               <InfiniteScroll
                 children={posts.results.map((post) => (
                   <Post key={post.id} {...post} setPosts={setPosts} />
-                ))
-            }
-            dataLength={posts.results.length}
-            loader={<Asset spinner />}
-            hasMore={!!posts.next}
-            next={() => fetchMoreData(posts, setPosts)}
+                ))}
+                dataLength={posts.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!posts.next}
+                next={() => fetchMoreData(posts, setPosts)}
               />
             ) : (
               <Container className={appStyles.Content}>
@@ -87,7 +87,7 @@ function PostsPage({ message, filter = "" }) {
         )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
+        <PopularProfiles />
       </Col>
     </Row>
   );
